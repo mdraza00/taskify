dca678f96c4db7c450eae1f5f5365e22b550dc32
Mohammed Raza <mohammedrraza26@gmail.com>
2024-11-26 06:28:45 +0530

    commit

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,37 +1,38 @@
-import {Entity, model, property} from '@loopback/repository';
+import { Entity, model, property } from "@loopback/repository";
 
 @model()
 export class User extends Entity {
   @property({
-    type: 'string',
+    type: "string",
     id: true,
     generated: true,
   })
   id?: string;
 
   @property({
-    type: 'string',
+    type: "string",
     required: true,
   })
   username: string;
 
   @property({
-    type: 'string',
+    type: "string",
     required: true,
   })
   email: string;
 
   @property({
-    type: 'string',
+    type: "string",
     required: true,
   })
   password: string;
 
   @property({
-    type: 'string',
+    type: "string",
+    default:
+      "https://i.pinimg.com/1200x/79/03/fa/7903fa4e4dd43aceb6694a87a8c7bed9.jpg",
   })
   profilePhoto?: string;
-
 
   constructor(data?: Partial<User>) {
     super(data);
