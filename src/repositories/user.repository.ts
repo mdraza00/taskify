c72390d2508eeb0cb1f8730e242505cb630bbfc2
Mohammed Raza <mohammedrraza26@gmail.com>
2024-11-27 08:39:28 +0530

    hash password

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -2,6 +2,8 @@ import { inject } from "@loopback/core";
 import { MongoDbDataSource } from "../datasources";
 import { User, UserRelations } from "../models";
 import { DefaultCrudRepository } from "@loopback/repository";
+import { generateHashPassword } from "../utils";
+
 export class UserRepository extends DefaultCrudRepository<
   User,
   typeof User.prototype.id,
@@ -14,12 +16,21 @@ export class UserRepository extends DefaultCrudRepository<
   async signup(user: Partial<User>): Promise<User> {
     const collection = (this.dataSource.connector as any).collection("User");
 
+    user.profilePhoto = user.profilePhoto
+      ? user.profilePhoto
+      : "https://i.pinimg.com/1200x/79/03/fa/7903fa4e4dd43aceb6694a87a8c7bed9.jpg";
+
+    const hashPassword = await generateHashPassword(user.password as string);
+    user.password = hashPassword;
+
     const result: any = await collection.insertOne(user);
+
     const userDoc = {
       ...result.ops[0],
       id: result.ops[0]._id,
     };
     delete userDoc._id;
+
     return userDoc as User;
   }
 }
--- src/utils/bcrypt-utils/bcrypt-utils.ts
+++ src/utils/bcrypt-utils/bcrypt-utils.ts
@@ -1,7 +1,12 @@
 import bcrypt from "bcrypt";
 
-export async function encryptPassword(password: string) {
+export async function generateHashPassword(password: string) {
   return await bcrypt.hash(password, 15);
 }
 
-export async function co
+export async function comparePassword(
+  hashedPassword: string,
+  password: string
+) {
+  return await bcrypt.compare(password, hashedPassword);
+}
